a39f4886ae52ace4253ae8d17df9c2deed15bae6
yliubyva <yliubyva@gmail.com>
2024-09-27 19:10:43 +0300

    add trimmed text

--- src/components/MainContent/SectionPendingReviews/ItemPendingReviews/ItemPendingReviews.jsx
+++ src/components/MainContent/SectionPendingReviews/ItemPendingReviews/ItemPendingReviews.jsx
@@ -3,13 +3,16 @@ import styles from "./ItemPendingReviews.module.css";
 import { Avatar } from "../../../Shared/Avatar/Avatar";
 
 export const ItemPendingReviews = ({ image, text }) => {
+    const maxLength = 50; 
+    const trimmedText = text.length > maxLength ? text.substring(0, maxLength) + '...' : text;
+
     return (
         <li className={styles.item}>
             <Avatar src={image} />
 
             <div className={styles.textContainer}>
                 <svg className={styles.icon}><use xlinkHref="#star"></use></svg>
-                <span className={styles.text}>{text}</span>
+                <span className={styles.text}>{trimmedText}</span>
             </div>
         </li>
     )
